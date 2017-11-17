9a8987af251490deec4da0f95a3c0b069dfe03ee
pablo <pablo0168@hotmail.com>
2017-11-16 20:54:57 -0600

    script talks to other clients

--- connect_log.py
+++ connect_log.py
@@ -3,17 +3,15 @@ from datetime import datetime
 import time
 import sys
 
-plot = [] # array of tuples of (success to ms or failures to ms
-
-CURR_TIME = datetime.now()
-DIFF = datetime.now() - CURR_TIME
-HOST = "8.8.8.8"
-#HOST = "192.168.1.4"
-
 
 
 def log_pings(TIME_LIMIT=5,HOST="8.8.8.8"):
-    while  DIFF.total_seconds() < TIME_LIMIT:# 60 seconds
+
+    plot = [] # array of tuples of (success to ms or failures to ms
+    CURR_TIME = datetime.now()
+    DIFF = datetime.now() - CURR_TIME
+
+    while  DIFF.total_seconds() < TIME_LIMIT:
 
         ret = None
 
@@ -23,7 +21,7 @@ def log_pings(TIME_LIMIT=5,HOST="8.8.8.8"):
             print(e)
 
         DIFF = datetime.now() - CURR_TIME
-        plot.append(DIFF.total_seconds(),ret)
+        plot.append((DIFF.total_seconds(),ret))
         time.sleep(.05)
 
 
@@ -39,6 +37,6 @@ if __name__ == "__main__":
     if len(sys.argv) == 3:
         _timeout = sys.argv[1]
         _host = sys.argv[2]
-        log_pings(_timeout, _host)
+        log_pings(TIME_LIMIT=int(_timeout), HOST=_host)
     else:
         log_pings()
