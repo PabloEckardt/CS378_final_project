dd53cc9555101e7b67a635fe8896cce88f1ad826
christine-h-yang <33558413+christine-h-yang@users.noreply.github.com>
2017-11-29 19:00:16 -0600

    Construct inverted index in parse_data.py

--- parse_data.py
+++ parse_data.py
@@ -1,31 +1,55 @@
 import csv
 import json
 
-with open('testcap-01.csv', newline='') as csvfile:
-	reader = csv.reader(csvfile)
+"""
+	DICTIONARY 1 FORMAT
 
-	"""
-		dictionary 1 format
-
-		victims {
-			ESSID {
-				channels {
-					BSSID {
-						Client1 mac
-							device name
-							optional data
-							...
-						Client2 mac
-							device name
-							optional data
-							...
+	essid_data {
+		ESSID {
+			channels {
+				BSSID {
+					Client1 mac
+						device name
+						optional data
 						...
-					}
+					Client2 mac
+						device name
+						optional data
+						...
+					...
 				}
 			}
 		}
-	"""
-	victims = {}
+	}
+
+	DICTIONARY 2 FORMAT
+
+	essid_data {
+		Client1 mac {
+			Device name
+			optional data
+			...
+			BSSIDS {
+				BSSID 1 {
+					Channel {
+						ESSID
+					}
+				}
+
+				BSSID 2 {
+					...
+				}
+			}
+		}
+	}
+
+"""
+essid_data = {}
+client_data = {}
+
+with open('testcap-01.csv', newline='') as csvfile:
+	reader = csv.reader(csvfile, skipinitialspace=True)
+
 	bssid_to_essid_and_channel = {}
 
 	# first empty row denotes the start of the access points table
@@ -44,39 +68,55 @@ with open('testcap-01.csv', newline='') as csvfile:
 		if table_num == 1:
 			bssid = row[0].strip()
 			channel = row[3].strip()
-			essid = row[13]
+			essid = row[13].strip()
 
 			bssid_to_essid_and_channel[bssid] = (essid, channel)
 
-			if essid in victims.keys():
-				if channel in victims[essid].keys():
-					victims[essid][channel][bssid] = {}
+			if essid in essid_data.keys():
+				if channel in essid_data[essid].keys():
+					essid_data[essid][channel][bssid] = {}
 					continue
 				bssid_dict = {}
 				bssid_dict[bssid] = {}
-				victims[essid][channel] =  bssid_dict
+				essid_data[essid][channel] =  bssid_dict
 			else:
 				bssid_dict = {}
 				bssid_dict[bssid] = {}
 				channel_dict = {}
 				channel_dict[channel] = bssid_dict
-				
-				victims[essid] = channel_dict
+
+				essid_data[essid] = channel_dict
 
 		elif table_num == 2:
 			mac = row[0].strip()
 			bssid = row[5].strip()
-			probed_essid = row[6]
+			#probed_essid = row[6].strip()
 
 			if bssid == '(not associated)':
 				continue
 
 			essid, channel = bssid_to_essid_and_channel[bssid]
 
-			mac_dict = {}
-			mac_dict['device name'] = probed_essid
-			mac_dict['operating system'] = ''
-			mac_dict['ports'] = []
-			victims[essid][channel][bssid] = mac_dict
+			mac_data = {}
+			mac_data['device name'] = ''
+			mac_data['operating system'] = ''
+			mac_data['ports'] = []
 
-	print(json.dumps(victims, indent=4))
+			essid_data[essid][channel][bssid][mac] = mac_data
+
+print(json.dumps(essid_data, indent=4))
+
+# Create dictionary 2 from dictionary 1
+for essid, channels in essid_data.items():
+	for channel, bssids in channels.items():
+		for bssid, clients in bssids.items():
+			for client_mac, mac_data in clients.items():
+				if client_mac not in client_data:
+					client_data[client_mac] = dict(mac_data)
+					client_data[client_mac]['bssids'] = {}
+				bssid_data = client_data[client_mac]['bssids']
+				if bssid not in bssid_data:
+					bssid_data[bssid] = {}
+				bssid_data[bssid][channel] = essid
+
+print(json.dumps(client_data, indent=4))
