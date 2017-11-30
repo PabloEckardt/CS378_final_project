7dabffb410d82f67d8edc7603341062c1bb2aefb
pablo <pablo0168@hotmail.com>
2017-11-29 23:02:24 -0600

    fixed poor design in my json schema on dict 2 where bssids were above channels, placed code inside functions

--- parse_data.py
+++ parse_data.py
@@ -2,121 +2,136 @@ import csv
 import json
 
 """
-	DICTIONARY 1 FORMAT
+    DICTIONARY 1 FORMAT
 
-	essid_data {
-		ESSID {
-			channels {
-				BSSID {
-					Client1 mac
-						device name
-						optional data
-						...
-					Client2 mac
-						device name
-						optional data
-						...
-					...
-				}
-			}
-		}
-	}
+    essid_data {
+        ESSID {
+            channels {
+                BSSID {
+                    Client1 mac
+                        device name
+                        optional data
+                        ...
+                    Client2 mac
+                        device name
+                        optional data
+                        ...
+                    ...
+                }
+            }
+        }
+    }
 
-	DICTIONARY 2 FORMAT
+    DICTIONARY 2 FORMAT
 
-	essid_data {
-		Client1 mac {
-			Device name
-			optional data
-			...
-			BSSIDS {
-				BSSID 1 {
-					Channel {
-						ESSID
-					}
-				}
+    essid_data {
+        Client1 mac {
+            Device name
+            optional data
+            ...
+            CHANNELS {
+                    CHANNEL{
+                        ESSID
+                    }
+                }
 
-				BSSID 2 {
-					...
-				}
-			}
-		}
-	}
+                BSSID 2 {
+                    ...
+                }
+            }
+        }
+    }
 
 """
-essid_data = {}
-client_data = {}
+def get_file_name():
+    # TODO write function that finds the csv file or whatever
+    return "testcap-01.csv"
 
-with open('testcap-01.csv', newline='') as csvfile:
-	reader = csv.reader(csvfile, skipinitialspace=True)
+def parse_network_packets(file_name):
+    essid_data = {}
+    with open('testcap-01.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, skipinitialspace=True)
+        bssid_to_essid_and_channel = {}
+        # first empty row denotes the start of the access points table
+        # second empty row denotes the start of the stations table
+        table_num = 0
+        skip_row = False
+        for row in reader:
+            if not row:
+                table_num += 1
+                skip_row = True
+                continue
+            if skip_row:
+                skip_row = False
+                continue
 
-	bssid_to_essid_and_channel = {}
+            if table_num == 1:
+                bssid = row[0].strip()
+                channel = row[3].strip()
+                essid = row[13].strip()
 
-	# first empty row denotes the start of the access points table
-	# second empty row denotes the start of the stations table
-	table_num = 0
-	skip_row = False
-	for row in reader:
-		if not row:
-			table_num += 1
-			skip_row = True
-			continue
-		if skip_row:
-			skip_row = False
-			continue
+                bssid_to_essid_and_channel[bssid] = (essid, channel)
 
-		if table_num == 1:
-			bssid = row[0].strip()
-			channel = row[3].strip()
-			essid = row[13].strip()
+                if essid in essid_data.keys():
+                    if channel in essid_data[essid].keys():
+                        essid_data[essid][channel][bssid] = {}
+                        continue
+                    bssid_dict = {}
+                    bssid_dict[bssid] = {}
+                    essid_data[essid][channel] =  bssid_dict
+                else:
+                    bssid_dict = {}
+                    bssid_dict[bssid] = {}
+                    channel_dict = {}
+                    channel_dict[channel] = bssid_dict
 
-			bssid_to_essid_and_channel[bssid] = (essid, channel)
+                    essid_data[essid] = channel_dict
 
-			if essid in essid_data.keys():
-				if channel in essid_data[essid].keys():
-					essid_data[essid][channel][bssid] = {}
-					continue
-				bssid_dict = {}
-				bssid_dict[bssid] = {}
-				essid_data[essid][channel] =  bssid_dict
-			else:
-				bssid_dict = {}
-				bssid_dict[bssid] = {}
-				channel_dict = {}
-				channel_dict[channel] = bssid_dict
+            elif table_num == 2:
+                mac = row[0].strip()
+                bssid = row[5].strip()
+                #probed_essid = row[6].strip()
 
-				essid_data[essid] = channel_dict
+                if bssid == '(not associated)':
+                    continue
 
-		elif table_num == 2:
-			mac = row[0].strip()
-			bssid = row[5].strip()
-			#probed_essid = row[6].strip()
+                essid, channel = bssid_to_essid_and_channel[bssid]
 
-			if bssid == '(not associated)':
-				continue
+                mac_data = {}
+                mac_data['device name'] = ''
+                mac_data['operating system'] = ''
+                mac_data['ports'] = []
 
-			essid, channel = bssid_to_essid_and_channel[bssid]
+                essid_data[essid][channel][bssid][mac] = mac_data
 
-			mac_data = {}
-			mac_data['device name'] = ''
-			mac_data['operating system'] = ''
-			mac_data['ports'] = []
+    return essid_data
 
-			essid_data[essid][channel][bssid][mac] = mac_data
 
-print(json.dumps(essid_data, indent=4))
+def invert_index(essid_data):
+    client_data = {}
+    for essid, channels in essid_data.items():
+        for channel, bssids in channels.items():
+            for bssid, clients in bssids.items():
+                for client_mac, mac_data in clients.items():
+                    if client_mac not in client_data:
+                        client_data[client_mac] = dict(mac_data)
+                        client_data[client_mac]['channels'] = {}
 
-# Create dictionary 2 from dictionary 1
-for essid, channels in essid_data.items():
-	for channel, bssids in channels.items():
-		for bssid, clients in bssids.items():
-			for client_mac, mac_data in clients.items():
-				if client_mac not in client_data:
-					client_data[client_mac] = dict(mac_data)
-					client_data[client_mac]['bssids'] = {}
-				bssid_data = client_data[client_mac]['bssids']
-				if bssid not in bssid_data:
-					bssid_data[bssid] = {}
-				bssid_data[bssid][channel] = essid
+                    if not channel in client_data[client_mac]['channels']:
+                        client_data[client_mac]['channels'][channel] = []
+                    client_data[client_mac]['channels'][channel].append(bssid)
+    return client_data
 
-print(json.dumps(client_data, indent=4))
+
+def display_json(j):
+    print (json.dumps(j, indent=4))
+
+
+if __name__ == "__main__":
+
+    packet_capture = get_file_name()
+    dict_1 = parse_network_packets(packet_capture)
+    dict_2 = invert_index(dict_1)
+
+    display_json(dict_1)
+    display_json(dict_2)
