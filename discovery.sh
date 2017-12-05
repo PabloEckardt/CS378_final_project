5ca9e41a48866f033ccdfcd0f676112a4ae96282
Raymond Chee <rjchee@gmail.com>
2017-12-05 16:40:30 -0600

    Update airodump call to be blocking

--- discovery.sh
+++ discovery.sh
@@ -5,7 +5,7 @@ discover() {
 #  airmon-ng check kill >/dev/null
 #  ifconfig eth0 down
 #  airmon-ng start wlan0 >/dev/null
-  gnome-terminal --working-directory=/tmp -e "timeout $TIMEOUT airodump-ng -a wlan0mon --output-format csv -w dump"
+  xterm +hold -e "cd /tmp && timeout $TIMEOUT airodump-ng -a wlan0mon --output-format csv -w dump"
 }
 
 discover
